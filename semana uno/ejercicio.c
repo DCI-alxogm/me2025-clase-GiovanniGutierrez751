9d2e60f35f032891a46adccb0478b371a2af08f3
GiovanniGutierrez751 <gioalguan@gmail.com>
2025-08-08 13:47:14 -0400

    Add files via upload

---
+++ semana uno/ejercicio.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+  float n;
+  float b;
+  float x [5];
+  x[0]=5.8;
+  x[1]=6.4;
+  x[2]=7.2;
+  x[3]=2.1;
+  x[4]=3.3;
+  n=log(x+1);
+  b=exp ()
+printf("%f %f %f",n)
+}

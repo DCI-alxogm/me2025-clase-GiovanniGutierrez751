47df5501a8e0caac89a6a5d9729fc554172fd675
GiovanniGutierrez751 <gioalguan@gmail.com>
2025-08-15 13:58:28 -0400

    Add files via upload

---
+++ Semana 2/problema1508 (1).c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <math.h>
+
+float a;
+float b;
+float c;
+float x1;
+float x2;
+printf("introduzca valores para a,b y c");
+scanf("%f %f %f\n",&a,&b,&c);
+if a==0;
+if b==0;
+printf("solución trivial");
+else;
+discr=b²-4ac;
+if discr>0

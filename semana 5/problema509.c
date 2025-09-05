bcc49f3d93863b90b7e3d8b04d790f1405f60921
GiovanniGutierrez751 <gioalguan@gmail.com>
2025-09-05 13:50:43 -0400

    Add files via upload

---
+++ semana 5/problema509.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <math.h>
+float fx(float x);
+int main(){
+float a=-2,b=2,h=0.25;
+int n=int((b-a)/h)+1;
+float x[n],fpa[n];
+for(i=0;i<n;i++){
+  x[i]=a+i*h;
+}
+for(i=0;i<n;i++){
+  fpa[i]=fx(x[i+1])-fx(x[i])/h;
+  printf("%f")
+}
+
+float fx(float x){
+  float f;
+  f=x*x*x-2*x+4
+  return f;
+}
+
+
